be6913b5505e7776323f783fbf0964449cd350b0
HeoSeonJin0504 <sonjin54@naver.com>
2024-04-10 14:59:05 +0900

    Added game resources

---
+++ app/src/main/java/flappybirdinjava/GameObject.java
@@ -0,0 +1,15 @@
+package flappybirdinjava;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class GameObject {
+    
+}
+
+class BackgroundPanel extends JPanel {
+    // 무엇을 만들 것인가? (목표)
+    // 우리가 무엇을 가져올 수 있는가? (입력)
+    // 가공을 하여 어떤 것을 줄 것인가? (출력)
+    
+}
