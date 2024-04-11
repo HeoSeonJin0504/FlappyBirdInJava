17e41329fd89443f5711de516640b346ac6cbb51
HeoSeonJin0504 <sonjin54@naver.com>
2024-04-11 15:54:08 +0900

    Created gamebackground

--- app/src/main/java/flappybirdinjava/Frame.java
+++ app/src/main/java/flappybirdinjava/Frame.java
@@ -3,14 +3,46 @@ package flappybirdinjava;
 import java.awt.*;
 import javax.swing.*;
 
+import com.google.common.primitives.Floats;
+import com.google.errorprone.annotations.OverridingMethodsMustInvokeSuper;
+
+import java.awt.event.*;
+
 public class Frame extends JFrame {
+    BackgroundPanel pnlBackground = new BackgroundPanel();
+
+    // Variable
+    private float sizeMultiply = 1.0f;
+    private final int ORIGIN_SIZE = 512;
+
     public Frame() {
         setTitle("Flappy Bird In Java");
-        setSize(600, 600);
+        setSize(512, 512);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        setMinimumSize(new Dimension(300, 300)); // Dimension - 유니티로 치면 Vector2 
+        setMinimumSize(new Dimension(256, 256)); // Dimension - 유니티로 치면 Vector2 
 
-        setLayout(null);
+        add(pnlBackground);
+
+    }
+
+    public float getSizeMultiply() {
+        return sizeMultiply;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        int width = getWidth();
+        int height = getHeight();
+
+        // width와 height 중 작은 값을 뽑아내고, 이걸 setSize w, h 변수에 집어 넣는다.
+        if (width > height) {
+             setSize (height, height);
+        }
+        else {
+            setSize(width, width);
+        }
+        sizeMultiply = (float)getWidth() / (float)ORIGIN_SIZE;
     }
 }
--- app/src/main/java/flappybirdinjava/GameObject.java
+++ app/src/main/java/flappybirdinjava/GameObject.java
@@ -11,5 +11,25 @@ class BackgroundPanel extends JPanel {
     // 무엇을 만들 것인가? (목표)
     // 우리가 무엇을 가져올 수 있는가? (입력)
     // 가공을 하여 어떤 것을 줄 것인가? (출력)
-    
+    Image imgBackground = new ImageIcon(Main.getPath("/sprites/background.png")).getImage();
+    private final int WIDTH = imgBackground.getWidth(this);
+    private final int HEIGHT= imgBackground.getHeight(this);
+
+    public BackgroundPanel() {
+        setLayout(null);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Frame frame = Main.getFrame();
+        float sizeMultiply = frame.getSizeMultiply();
+        int fixedWidth = (int)(WIDTH * sizeMultiply);
+        int fixedHeight = (int)(HEIGHT * sizeMultiply);
+
+        for (int i = 0; i<frame.getWidth() / fixedWidth + 1; i++) {
+            g.drawImage(imgBackground, i * fixedWidth, 0, fixedWidth, fixedHeight, this);
+        }
+    }
 }
